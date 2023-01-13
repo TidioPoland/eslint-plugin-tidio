c5081484aba1dc65096c5547ab6b31c8653fa410
Daniel Domański <hello@danieldomanski.com>
2023-01-13 18:44:00 +0100

    Add packages required to run additional rules and relative tests

---
+++ src/tests/rules/lang-should-have-absolute-import.js
@@ -0,0 +1,71 @@
+/**
+ * @fileoverview Lang import should have an absolute import
+ * @author lang-should-have-absolute-import
+ */
+'use strict';
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require('../../rules/lang-should-have-absolute-import'),
+  RuleTester = require('eslint').RuleTester;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+RuleTester.setDefaultConfig({
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module'
+  }
+});
+var ruleTester = new RuleTester();
+ruleTester.run('lang-should-have-absolute-import', rule, {
+  valid: [
+    "import trans from 'lang'",
+    "import trans from './lang'",
+    "import { doesTranslationExist } from 'lang';",
+    "import trans, { doesTranslationExist } from 'lang';",
+    "import nothingReally from '../modals/langDE';",
+    "import nothingReally from '../langPL';"
+  ],
+
+  invalid: [
+    {
+      code: `import trans from '../lang';`,
+      errors: [
+        {
+          message: 'lang module import should be absolute',
+          type: 'ImportDeclaration'
+        }
+      ]
+    },
+    {
+      code: `import trans, { doesTranslationExist } from '../lang';`,
+      errors: [
+        {
+          message: 'lang module import should be absolute',
+          type: 'ImportDeclaration'
+        }
+      ]
+    },
+    {
+      code: `import { doesTranslationExist } from '../lang';`,
+      errors: [
+        {
+          message: 'lang module import should be absolute',
+          type: 'ImportDeclaration'
+        }
+      ]
+    },
+    {
+      code: `import trans from '../../../../lang';`,
+      errors: [
+        {
+          message: 'lang module import should be absolute',
+          type: 'ImportDeclaration'
+        }
+      ]
+    }
+  ]
+});
---
+++ src/tests/rules/require-const-in-function-return.js
@@ -0,0 +1,65 @@
+'use strict';
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require('../../rules/require-const-in-action-creator-ts-function'),
+  RuleTester = require('eslint').RuleTester;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+RuleTester.setDefaultConfig({
+  parser: require.resolve('@typescript-eslint/parser'),
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+  },
+});
+var ruleTester = new RuleTester();
+ruleTester.run('require-const-in-action-creator-ts-function', rule, {
+  valid: [
+    {
+      code: `export function fetchRegularCampaigns() {return <const>{type: "SOME-TYPE-ZIOM"};}`,
+      filename: 'actions.ts',
+    },
+    {
+      code: `export function fetchRegularCampaigns() {return <const>{type: "SOME-TYPE"};}`,
+      filename: 'actionsTS.ts',
+    },
+    {
+      code: `export function fetchRegularCampaigns() {return {type: "SOME-TYPE"};}`,
+      filename: 'actions.js',
+    },
+    {
+      code: `export function fetchRegularCampaigns() {return {type: "SOME-TYPE"};}`,
+      filename: 'some/path/with/actions/file.js',
+    },
+    {
+      code: `export function saveNotifications(
+        hasNotificationSoundChanged: boolean,
+    ) {
+        const emptyFunc = (): void => {};
+        return <const>{
+            type: OPERATORS_SAVE_NOTIFICATIONS,
+            hasNotificationSoundChanged,
+        };
+    }`,
+      filename: 'actions.ts',
+    },
+  ],
+
+  invalid: [
+    {
+      code: 'export function fetchRegularCampaigns() {return {type: "SOME-TEST-TYPE"};}',
+      filename: 'actionsTS.ts',
+      errors: [
+        {
+          message:
+            'Return in TS action creator should use <const> before returned object',
+          type: 'ReturnStatement',
+        },
+      ],
+    },
+  ],
+});
