01d5ab527abfc764c001143e69fd156af0bd6722
Jarosław Salwa <jaroslaw@tidio.net>
2023-02-27 13:45:57 +0100

    Use new additional-rules package

--- src/translations.js
+++ src/translations.js
@@ -1,8 +1,8 @@
 const translations = {
-  plugins: ["eslint-plugin-tidio-additional-rules"],
+  plugins: ["@tidio/eslint-plugin-tidio-additional-rules"],
   rules: {
-    "tidio-additional-rules/import-from-lang-always-trans": 2,
-    "tidio-additional-rules/eslint-plugin-disallow-literals-as-direct-jsxelement-children": 2,
+    "@tidio/tidio-additional-rules/import-from-lang-always-trans": 2,
+    "@tidio/tidio-additional-rules/eslint-plugin-disallow-literals-as-direct-jsxelement-children": 2,
   },
 };
 
