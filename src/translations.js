6ff04cc3fca8cdc72712c4eb681a52fb50c23316
Jarosław Salwa <jaroslaw@tidio.net>
2025-10-08 10:31:36 +0200

    Add no trans in tests rule

--- src/translations.js
+++ src/translations.js
@@ -3,6 +3,7 @@ const translations = {
   rules: {
     "@tidio/rules/import-from-lang-always-trans": 2,
     "@tidio/rules/eslint-plugin-disallow-literals-as-direct-jsxelement-children": 2,
+    "@tidio/rules/no-trans-in-tests": 2,
   },
 };
 
