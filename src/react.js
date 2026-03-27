61166b70953131485dc9793d01546dc3ffd8f429
Andrzej Chuchra <a.chuchra@tidio.net>
2026-03-27 16:04:27 +0100

    bump eslint and packages, update config files and README.md

--- src/react.js
+++ src/react.js
@@ -1,91 +1,95 @@
-const basic = require("./basic");
+import { FlatCompat } from "@eslint/eslintrc";
+import tidioRulesPlugin from "@tidio/eslint-plugin-rules";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import { coreRules } from "./basic.js";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
 
-const react = {};
-react.plugins = Object.assign([], basic.plugins);
-react.plugins.push("eslint-plugin-react-compiler", "react-hooks", "react");
-
-react.extends = Object.assign([], basic.extends);
-react.extends = react.extends.map((extend) => {
-  if (extend === "airbnb-base") {
-    return "airbnb";
-  }
-  return extend;
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
 });
 
-react.settings = Object.assign({}, basic.settings);
-const reactRules = {
-  // React
-  "@tidio/rules/imports-order-for-less-files": 2,
-  "@tidio/rules/imports-order-for-media-files": 2,
-  "@tidio/rules/eslint-plugin-disallow-null-in-tenary-in-jsx": 2,
-  "react-hooks/rules-of-hooks": "error",
-  "react-hooks/exhaustive-deps": "error",
-  "react/no-danger": 2,
-  "jsx-a11y/label-has-for": [
-    2,
-    {
-      required: {
-        some: ["nesting", "id"],
-      },
-      allowChildren: true,
-    },
-  ],
-  "jsx-a11y/label-has-associated-control": [
-    "error",
-    {
-      labelComponents: [],
-      labelAttributes: [],
-      controlComponents: [],
-      assert: "either",
-      depth: 25,
-    },
-  ],
-  "react/destructuring-assignment": 0,
-  "react/state-in-constructor": 0, // we are using babel so it is not necessary for us
-  "jsx-a11y/control-has-associated-label": 0,
-  "react/jsx-props-no-spreading": 0, // allow spreading
-  "react/jsx-filename-extension": [
-    2,
-    {
-      extensions: [".jsx", ".tsx"],
-    },
-  ],
+const react = [
+  ...compat.extends("airbnb"),
+  ...coreRules,
 
-  "react/function-component-definition": [
-    2,
-    {
-      namedComponents: "arrow-function",
-      unnamedComponents: "function-expression",
-    },
-  ],
-  "react/jsx-no-useless-fragment": [
-    2,
-    {
-      allowExpressions: true,
-    },
-  ],
-  // Disable react/sort-comp as this is not necessary for us and fixing it can cause many errors
-  "react/sort-comp": 0,
-  "react-compiler/react-compiler": 2,
-};
-
-react.rules = Object.assign({}, basic.rules, reactRules);
-
-const reactOverrides = [
   {
-    files: ["**/*.jsx", "*.tsx"],
+    plugins: {
+      "@tidio/rules": tidioRulesPlugin,
+      "react-hooks": reactHooksPlugin,
+    },
+    rules: {
+      "@tidio/rules/imports-order-for-less-files": 2,
+      "@tidio/rules/imports-order-for-media-files": 2,
+      "@tidio/rules/eslint-plugin-disallow-null-in-tenary-in-jsx": 2,
+
+      "react/no-danger": 2,
+      "react/destructuring-assignment": 0,
+      "react/state-in-constructor": 0,
+      "react/jsx-props-no-spreading": 0,
+      "react/jsx-filename-extension": [2, { extensions: [".jsx", ".tsx"] }],
+      "react/function-component-definition": [
+        2,
+        {
+          namedComponents: "arrow-function",
+          unnamedComponents: "function-expression",
+        },
+      ],
+      "react/jsx-no-useless-fragment": [2, { allowExpressions: true }],
+      "react/sort-comp": 0,
+
+      "jsx-a11y/label-has-for": [
+        2,
+        {
+          required: { some: ["nesting", "id"] },
+          allowChildren: true,
+        },
+      ],
+      "jsx-a11y/label-has-associated-control": [
+        "error",
+        {
+          labelComponents: [],
+          labelAttributes: [],
+          controlComponents: [],
+          assert: "either",
+          depth: 25,
+        },
+      ],
+      "jsx-a11y/control-has-associated-label": 0,
+
+      "react-hooks/component-hook-factories": 2,
+      "react-hooks/config": 2,
+      "react-hooks/error-boundaries": 2,
+      "react-hooks/gating": 2,
+      "react-hooks/globals": 2,
+      "react-hooks/immutability": 2,
+      "react-hooks/incompatible-library": 2,
+      "react-hooks/preserve-manual-memoization": 2,
+      "react-hooks/purity": 1,
+      // False positive — https://github.com/facebook/react/issues/34775
+      "react-hooks/refs": 0,
+      "react-hooks/set-state-in-effect": 2,
+      "react-hooks/set-state-in-render": 2,
+      // We very often pass components during rendering
+      "react-hooks/static-components": "warn",
+      "react-hooks/unsupported-syntax": 1,
+      "react-hooks/use-memo": 2,
+    },
+  },
+
+  {
+    files: ["**/*.jsx", "**/*.tsx"],
     rules: {
       "@tidio/rules/always-require-default-export": 2,
     },
   },
   {
     files: [
-      "*.mocks.jsx",
-      "*.mocks.tsx",
-      "*.styles.jsx",
-      "*.styles.tsx",
-      "styles.jsx",
-      "styles.tsx",
+      "**/*.mocks.jsx",
+      "**/*.mocks.tsx",
+      "**/*.styles.jsx",
+      "**/*.styles.tsx",
+      "**/styles.jsx",
+      "**/styles.tsx",
     ],
     rules: {
       "@tidio/rules/always-require-default-export": 0,
@@ -94,24 +98,13 @@ const reactOverrides = [
   {
     files: ["**/*.tsx", "**/*.ts"],
     rules: {
-      "react/require-default-props": [
-        0,
-        {
-          forbidDefaultForRequired: false,
-          ignoreFunctionalComponents: true,
-        },
-      ],
-      "react/default-props-match-prop-types": [
-        0,
-        {
-          allowRequiredDefaults: true,
-        },
-      ],
+      "react/require-default-props": 0,
+      "react/default-props-match-prop-types": 0,
       "react/prop-types": 0,
     },
   },
+
+  eslintPluginPrettierRecommended,
 ];
 
-react.overrides = [...basic.overrides, ...reactOverrides];
-
-module.exports = react;
+export default react;
--- src/translations.js
+++ src/translations.js
@@ -1,10 +1,16 @@
-const translations = {
-  plugins: ["@tidio/eslint-plugin-rules"],
-  rules: {
-    "@tidio/rules/import-from-lang-always-trans": 2,
-    "@tidio/rules/eslint-plugin-disallow-literals-as-direct-jsxelement-children": 2,
-    "@tidio/rules/no-trans-in-tests": 2,
-  },
-};
+import tidioRulesPlugin from "@tidio/eslint-plugin-rules";
 
-module.exports = translations;
+const translations = [
+  {
+    plugins: {
+      "@tidio/rules": tidioRulesPlugin,
+    },
+    rules: {
+      "@tidio/rules/import-from-lang-always-trans": 2,
+      "@tidio/rules/eslint-plugin-disallow-literals-as-direct-jsxelement-children": 2,
+      "@tidio/rules/no-trans-in-tests": 2,
+    },
+  },
+];
+
+export default translations;
